1fbd29e35b278f0c28aaadadf2578551a0201693
lai2-24 <lai2-24@itismeucci.com>
2025-04-03 14:33:19 +0200

    ottimizato

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 const aggiungi = document.getElementById("aggiungi");
 const azzera = document.getElementById("azzera");
 const container = document.querySelector(".container");
+let id;
+
 
 aggiungi.addEventListener("click", async () => {
 
     const risposta = await fetch("https://it.wikipedia.org/w/api.php?action=query&list=random&rnlimit=1&rnnamespace=0&format=json&origin=*");
     const data = await risposta.json();
 
-    const id = data.query.random[0].id;
+    id = data.query.random[0].id;
 
     const secondRequest = await fetch(`https://it.wikipedia.org/w/api.php?action=query&prop=extracts&explaintext&exintro&format=json&origin=*&pageids=${id}`);
     const infoArticolo = await secondRequest.json();
+
+    creaArticolo(infoArticolo);
+
+});
+
+
+function creaArticolo(infoA){
 
     const articolo = document.createElement("article");
     const paragrafo = document.createElement("p");
     const titolo = document.createElement("h3");
 
-    paragrafo.innerHTML = infoArticolo.query.pages[id].extract;
-    titolo.innerHTML = infoArticolo.query.pages[id].title;
+    paragrafo.innerHTML = infoA.query.pages[id].extract;
+    titolo.innerHTML = infoA.query.pages[id].title;
 
     articolo.append(titolo, paragrafo);
 
     container.append(articolo);
 
-
-});
+}
 
 azzera.addEventListener("click", () => {
 
